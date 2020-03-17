cf432f04b772d52b0ea5ef30c9ac3660e9ecded1
UO263909 <UO263909@uniovi.es>
2020-03-17 16:41:33 +0100

    SDI-NodeJS- 8,9,10 Recursos estáticos, Peticiones POST y parámetros, Enrutamiento y comodines

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -4,15 +4,25 @@ module.exports = function (app) {
     });
 
 
-    app.get('/canciones/:id', function (req, res) {
+    app.get("/canciones/:id", function (req, res) {
         let respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
     });
 
-    app.get('/canciones/:genero/:id', function (req, res) {
+    app.get("/canciones/:genero/:id", function (req, res) {
         let respuesta = 'id: ' + req.params.id + '<br>'
             + 'Género: ' + req.params.genero;
         res.send(respuesta);
     });
+
+    app.post('/cancion', function (req, res) {
+        let respuesta = "Canción agregada: " + req.body.nombre + '<br>'
+            + 'Género: ' + req.body.genero + '<br>' +"Precio: " + req.body.precio;
+        res.send(respuesta);
+    });
+    app.get('/promo*', function (req, res) {
+        res.send('Respuesta patrón promo* ');
+    })
+
 
 };
