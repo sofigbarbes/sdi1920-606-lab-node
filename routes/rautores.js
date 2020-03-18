2a9110ba7293e8fef17563a88b93e113ccb86d0f
UO263909 <UO263909@uniovi.es>
2020-03-18 13:53:52 +0100

    SDI–session7-Ejercicio-complementario2

--- routes/rautores.js
+++ routes/rautores.js
@@ -4,29 +4,28 @@ module.exports = function (app, swig) {
         res.send("ver canciones");
     });
 */
+    /*
     app.get('/autores/agregar', function (req, res) {
         let respuesta = swig.renderFile('views/bautores-agregar.html', {});
         res.send(respuesta);
     })
-
+*/
     app.post('/autor', function (req, res) {
         let respuesta = "Autor agregado: " + req.body.nombre + '<br>'
             + 'Grupo: ' + req.body.grupo + '<br>' + "Rol: " + req.body.rol;
 
         respuesta = "";
-        if(req.body.nombre===""){
+        if (req.body.nombre === "") {
             respuesta += 'Nombre no enviado en la petición' + '<br>';
+        } else {
+            respuesta += "Nombre: " + req.body.nombre + '<br>';
         }
-        else{
-            respuesta+= "Nombre: " + req.body.nombre+ '<br>';
-        }
-        if(req.body.grupo===""){
+        if (req.body.grupo === "") {
             respuesta += 'Grupo no enviado en la petición' + '<br>';
+        } else {
+            respuesta += "Grupo: " + req.body.grupo + '<br>';
         }
-        else{
-            respuesta+= "Grupo: " + req.body.grupo+ '<br>';
-        }
-        respuesta+= "Rol: " + req.body.rol+ '<br>';
+        respuesta += "Rol: " + req.body.rol + '<br>';
         res.send(respuesta);
     });
 
@@ -52,6 +51,22 @@ module.exports = function (app, swig) {
         });
         res.send(respuesta);
     });
+
+
+    app.get('/autores/agregar', function (req, res) {
+        let roles = [{"nombre": "Cantante"},
+            {"nombre": "Batería"},
+            {"nombre": "Bajista"},
+            {"nombre": "Teclista"},
+            {"nombre": "Guitarrista"}];
+
+        let respuesta = swig.renderFile('views/autores-agregar.html', {
+            roles: roles
+        });
+        res.send(respuesta);
+    });
+
+
     app.get('/autores/*', function (req, res) {
         res.redirect("/autores");
     })
