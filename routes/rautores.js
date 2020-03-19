d4dfd7304e607431467dfae824c2dcdd13cb3e71
UO263909 <UO263909@uniovi.es>
2020-03-19 18:41:28 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- routes/rautores.js
+++ routes/rautores.js
@@ -1,4 +1,4 @@
-module.exports = function (app, swig) {
+module.exports = function (app, swig, gestorBD) {
     /*
     app.get("/canciones", function (req, res) {
         res.send("ver canciones");
