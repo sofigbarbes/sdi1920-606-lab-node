62b528fce97c13a7c7c164aa8970c951babd5051
UO263909 <UO263909@uniovi.es>
2020-04-10 16:24:59 +0200

    SDI-NodeJS-10.1- SW API REST CRUD Canciones

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, 
 require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcomentarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rapicanciones.js")(app, gestorBD);
 
 // lanzar el servidor
 https.createServer({
